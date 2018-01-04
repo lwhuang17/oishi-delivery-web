478c4242ebdbd4d1e5d5385fa2d08c5f07c13f00
David Huang <david.huang@logicsolutions.com>
2018-01-03 22:24:48 -0500

    two way binding for customer name

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    this.state = {
+      name: ''
+    };
+  }
+
+  handleChange(event) {
+    const name = event.target.value;
+    this.setState({ name });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Oishi Delivery</h1>
+          <h1 className="App-title">Hello {this.state.name}, welcome to Oishi Delivery</h1>
         </header>
         <p className="App-intro">
           Your orders will be quickly processed. Enjoy!
         </p>
+        Name: <input autoFocus type="text" placeholder="Enter Name" name="name" value={this.state.name} onChange={this.handleChange.bind(this)}></input>
       </div>
     );
   }
